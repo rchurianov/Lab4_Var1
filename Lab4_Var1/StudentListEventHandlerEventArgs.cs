89aa151268aebc411a0c6ed07ec96eebdd25eb09
rchurianov <rchurianov@gmail.com>
2017-03-02 12:07:24 +0300

    StudentListEventHandlerArgs
    added simple args class

---
+++ Lab4_Var1/StudentListEventHandlerEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4_Var1
+{
+    public class StudentListEventHandlerEventArgs : EventArgs
+    {
+        public string CollectionName
+        { get; set; }
+
+        public string ChangeType
+        { get; set; }
+
+        public Student ChangedObject
+        { get; set; }
+
+        public override string ToString()
+        {
+            return CollectionName + "\n" +
+                ChangeType + "\n" +
+                ChangedObject + "\n";
+        }
+    }
+}
