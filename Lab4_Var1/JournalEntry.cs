d726655782a99abffbd7110ec1ee459a4b089779
rchurianov <rchurianov@gmail.com>
2017-03-04 17:51:30 +0300

    JournalEntry - added constructor with parameters

--- Lab4_Var1/JournalEntry.cs
+++ Lab4_Var1/JournalEntry.cs
@@ -8,11 +8,23 @@ namespace Lab4_Var1
 {
     public class JournalEntry
     {
+        /* Default constructor. Initializes fields with empty strings. */
         public JournalEntry()
         {
             this.ChangedCollectionName = "";
             this.ChangeType = "";
             this.StudentObjectString = "";
+        }
+
+        /* First argument - name of the collection which was changed.
+         * Second argument - a string that states what was changed.
+         * Third argument - a string with data from the element of collection that was changed.
+         */
+        public JournalEntry(string collection_name, string change_type, string student)
+        {
+            this.ChangedCollectionName = collection_name;
+            this.ChangeType = change_type;
+            this.StudentObjectString = student;
         }
 
         public string ChangedCollectionName { get; set; }
