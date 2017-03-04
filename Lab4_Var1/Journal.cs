c78ed5cc22fab292d37170f160531562b12e3180
rchurianov <rchurianov@gmail.com>
2017-03-04 17:52:55 +0300

    Journal - added constructor, event handlers, ToString
    Event handlers handle events from StudentCollection. On occasion on each of the events a Journal entry is created and added to internal collection.

---
+++ Lab4_Var1/Journal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Var1
+{
+    public class Journal
+    {
+        private List<JournalEntry> entries;
+
+        public Journal()
+        {
+            entries = new List<JournalEntry>();
+        }
+
+        /* Event handler for StudentsCountChanged event from StudentCollection. */
+        public void handle_StudentsCountChanged(object sender, StudentListEventHandlerEventArgs args)
+        {
+            JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.ChangedObject.ToString());
+            entries.Add(je);
+        }
+
+        /* Event handler for StudentReferenceChanged event from StudentCollection. */
+        public void handle_StudentReferenceChanged(object sender, StudentListEventHandlerEventArgs args)
+        {
+            JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.ChangedObject.ToString());
+            entries.Add(je);
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            if (entries != null)
+            {
+                foreach(JournalEntry entry in entries)
+                {
+                    s += entry.ToString() + "\n";
+                }
+            }
+            return s;
+        }
+    }
+}
