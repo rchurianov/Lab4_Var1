410b1d911852cc7c24a2e67d6d1252d0718b0875
rchurianov <rchurianov@gmail.com>
2017-03-04 18:37:26 +0300

    Program - fullfilled requrements

--- Lab4_Var1/Program.cs
+++ Lab4_Var1/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace Lab4_Var1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            StudentCollection computer_vision_group = new StudentCollection();
+            StudentCollection bayes_methods_group = new StudentCollection();
+            Journal vision_journal = new Journal();
+            Journal references_journal = new Journal();
+
+            computer_vision_group.StudentsCountChanged += vision_journal.handle_StudentsCountChanged;
+            computer_vision_group.StudentReferenceChanged += vision_journal.handle_StudentReferenceChanged;
+
+            computer_vision_group.StudentReferenceChanged += references_journal.handle_StudentReferenceChanged;
+            bayes_methods_group.StudentReferenceChanged += references_journal.handle_StudentReferenceChanged;
+
+            computer_vision_group.AddDefaults();
+            bayes_methods_group.AddDefaults();
+
+            computer_vision_group.Remove(2);
+
+            bayes_methods_group[1] = new Student(new Person("Name", "LAST NAME", new DateTime(1233, 1, 1)), Education.SecondEducation, 333);
+
+            Console.WriteLine(vision_journal.ToString());
+            Console.WriteLine("____________\n");
+            Console.WriteLine(references_journal.ToString());
+            Console.ReadKey();
         }
     }
 }
