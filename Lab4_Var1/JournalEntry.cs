c52de39958fdf323ce9c5390ff8965f435f0e8dd
rchurianov <rchurianov@gmail.com>
2017-03-04 17:34:18 +0300

    JournalEntry - added class

---
+++ Lab4_Var1/JournalEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Var1
+{
+    public class JournalEntry
+    {
+        public JournalEntry()
+        {
+            this.ChangedCollectionName = "";
+            this.ChangeType = "";
+            this.StudentObjectString = "";
+        }
+
+        public string ChangedCollectionName { get; set; }
+
+        public string ChangeType { get; set; }
+
+        public string StudentObjectString { get; set; }
+
+        public override string ToString()
+        {
+            return ChangedCollectionName + "\n" +
+                ChangeType + "\n" +
+                StudentObjectString;
+        }
+    }
+}
