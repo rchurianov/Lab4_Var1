eca7140951b8ef7b0f1ac667c072cbbba03f68a2
rchurianov <rchurianov@gmail.com>
2017-03-07 20:54:40 +0300

    Action - added enum with values "Add", "Remove", "Property"

---
+++ Lab4_Var1/ActionEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Var1
+{
+    enum Action { Add, Remove, Property };
+}
