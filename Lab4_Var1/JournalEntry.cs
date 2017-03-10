148d0f0e8bc32f65c96a65b68053830909cfea2b
rchurianov <rchurianov@gmail.com>
2017-03-10 19:40:11 +0300

    JournalEntry - changed to new requirements
    * removed default constructor as unnecessary
    * added StudentPropertyName and ChangedObjectKey - properties
    * changed constructor accordinly
    * changed ToString accordingly

--- Lab4_Var1/JournalEntry.cs
+++ Lab4_Var1/JournalEntry.cs
@@ -9,35 +9,39 @@ namespace Lab4_Var1
     public class JournalEntry
     {
         /* Default constructor. Initializes fields with empty strings. */
-        public JournalEntry()
-        {
-            this.ChangedCollectionName = "";
-            this.ChangeType = "";
-            this.StudentObjectString = "";
-        }
+        //public JournalEntry()
+        //{
+        //    this.ChangedCollectionName = "";
+        //    this.ChangeType = "";
+        //    this.StudentObjectString = "";
+        //}
 
         /* First argument - name of the collection which was changed.
          * Second argument - a string that states what was changed.
          * Third argument - a string with data from the element of collection that was changed.
          */
-        public JournalEntry(string collection_name, string change_type, string student)
+        public JournalEntry(string collection_name, Action change_type, string property_name, string key)
         {
             this.ChangedCollectionName = collection_name;
             this.ChangeType = change_type;
-            this.StudentObjectString = student;
+            this.StudentPropertyName = property_name;
+            this.ChangedObjectKey = key;
         }
 
         public string ChangedCollectionName { get; set; }
 
-        public string ChangeType { get; set; }
+        public Action ChangeType { get; set; }
 
-        public string StudentObjectString { get; set; }
+        public string StudentPropertyName { get; set; }
+
+        public string ChangedObjectKey { get; set; }
 
         public override string ToString()
         {
             return ChangedCollectionName + "\n" +
-                ChangeType + "\n" +
-                StudentObjectString;
+                ChangeType.ToString() + "\n" +
+                StudentPropertyName + "\n" +
+                ChangedObjectKey;
         }
     }
 }
