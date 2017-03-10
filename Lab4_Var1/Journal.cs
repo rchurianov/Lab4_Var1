aa297a0559798bcf977ba31c8d3876f787005bf8
rchurianov <rchurianov@gmail.com>
2017-03-10 20:19:11 +0300

    Journal - changed accordig to new requirements

--- Lab4_Var1/Journal.cs
+++ Lab4_Var1/Journal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,14 +19,20 @@ namespace Lab4_Var1
         /* Event handler for StudentsCountChanged event from StudentCollection. */
         public void handle_StudentsCountChanged(object sender, StudentListEventHandlerEventArgs args)
         {
-            JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.ChangedObject.ToString());
-            entries.Add(je);
+            //JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.ChangedObject.ToString());
+            //entries.Add(je);
         }
 
         /* Event handler for StudentReferenceChanged event from StudentCollection. */
         public void handle_StudentReferenceChanged(object sender, StudentListEventHandlerEventArgs args)
         {
-            JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.ChangedObject.ToString());
+            //JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.ChangedObject.ToString());
+            //entries.Add(je);
+        }
+
+        public void handle_StudentsChanged(object sender, StudentsChangedEventArgs<string> args)
+        {
+            JournalEntry je = new JournalEntry(args.CollectionName, args.ChangeType, args.StudentProperty, args.ChangedElementKey.ToString());
             entries.Add(je);
         }
 
