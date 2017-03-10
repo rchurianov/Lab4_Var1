0b2d788cc34a145c9c9103bbc4dce573ad14cdbf
rchurianov <rchurianov@gmail.com>
2017-03-10 17:27:27 +0300

    StudentsChangedHandler - addede delegate

---
+++ Lab4_Var1/StudentsChangedHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Var1
+{
+    public delegate void StudentsChangedHandler<TKey>(object source, StudentsChangedEventArgs<TKey> args);
+}
